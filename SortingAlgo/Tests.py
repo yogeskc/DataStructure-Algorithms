51c45568638510fd1b4c64730ab4bd9d3479b689
ycasey71 <ycasey71@gmail.com>
2023-04-01 15:01:45 -0700

    modified SortingAlgo/Tests.py

--- SortingAlgo/Tests.py
+++ SortingAlgo/Tests.py
@@ -1,42 +1,36 @@
-def bubbleSort(myList): 
-    for i in range(len(myList)-1,0,-1): 
+def BubbleSort(myList): 
+    for i in range(len(myList)-1, 0, -1): 
         for j in range(i): 
-            if myList[j] > myList[j+1]: 
+            if myList[j] > myList[j+1]:
                 temp = myList[j]
                 myList[j] = myList[j+1]
-                myList[j+1]= temp
+                myList[j+1] = temp
                 
     return myList
 
 
-def selectionSort(myList): 
-    for i in range(len(myList)-1): 
+def selectionSort(mylist): 
+    for i in range(len(mylist)-1):
         min_index = i 
-        for j in range(i+1,len(myList)): 
-            if myList[j] < myList[min_index]: 
+        for j in range(i+1, len(mylist) ):
+            if mylist[j] < mylist[min_index]: 
                 min_index = j 
-        
         if i != min_index: 
-            temp = myList[i]
-            myList[i] = myList[min_index]
-            myList[min_index] = temp 
-    return myList
-            
-
-
-def insertionSort(mylist): 
-    for i in range(1,len(mylist)): 
-        temp = mylist[i]
-        j = i -1 
-        
-        while temp < mylist[j] and j>-1: 
-            mylist[j+1] = mylist[j]
-            mylist[j]  = temp 
-            j -= 1
-            
+            temp = mylist[i]
+            mylist[i] = mylist[min_index]
+            mylist[min_index] = temp
     return mylist
-            
+        
+        
+def insertion_sort(myList): 
+    for i in range(len(myList)): 
+        temp = myList[i]
+        j = i - 1
+        while temp < myList[j] and j > -1: 
+            myList[j+1] = myList[j]
+            myList[j] = temp
+            j-=1
+    return myList
 
-
-print(insertionSort([121,12,3,4,33,21]))
-
+abc = [15434,1454,123112,534,12312,1,123,1123]
+print(insertion_sort(abc))
